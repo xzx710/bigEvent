aac664d16504580e44fe077a8a7d4536895009db
xiezexuan <2974758363@qq.com>
2022-11-17 19:33:08 +0800

    个人中心模块完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,26 @@
 $(function () {
   var layer = layui.layer
+  // 点击按钮，实现退出功能
+  $("#btnLogout").on("click", function () {
+    // 提示用户是否确认退出
+    layer.confirm(
+      "确定退出登录?",
+      { icon: 3, title: "提示" },
+      function (index) {
+        //do something
+        // 1. 清空本地存储中的 token
+        localStorage.removeItem("token")
+        // 2. 重新跳转到登录页面
+        location.href = "/login.html"
+        // 关闭 confirm 询问框
+        layer.close(index)
+      }
+    )
+  })
+
+  getUserInfo()
+})
+
 
   // 获取用户的基本信息
   function getUserInfo() {
@@ -37,24 +58,3 @@ $(function () {
       $(".text-avatar").html(first).show()
     }
   }
-
-  // 点击按钮，实现退出功能
-  $("#btnLogout").on("click", function () {
-    // 提示用户是否确认退出
-    layer.confirm(
-      "确定退出登录?",
-      { icon: 3, title: "提示" },
-      function (index) {
-        //do something
-        // 1. 清空本地存储中的 token
-        localStorage.removeItem("token")
-        // 2. 重新跳转到登录页面
-        location.href = "/login.html"
-        // 关闭 confirm 询问框
-        layer.close(index)
-      }
-    )
-  })
-
-  getUserInfo()
-})
