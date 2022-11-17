3f1b39e877f36534e30c08d117079e2777856354
xiezexuan <2974758363@qq.com>
2022-11-17 17:43:30 +0800

    主页部分完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,3 +1,21 @@
 $.ajaxPrefilter(function (options) {
+  // 统一拼接真实的url
   options.url = "http://big-event-api-t.itheima.net" + options.url
+
+  // 统一为有权限的接口，设置 headers 请求头
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    }
+  }
+
+  // 全局统一挂载 complete 回调函数
+  options.complete = function(res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+    // 1. 强制清空 token
+    localStorage.removeItem('token')
+    // 2. 强制跳转到登录页面
+    location.href = '/login.html'
+    }
+    }
 })
